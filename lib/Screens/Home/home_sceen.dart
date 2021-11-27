01a747c0bfc0dd026fb22de827ef91a45b8b1afc
hp <mahamudul951@gmail.com>
2021-11-27 12:34:52 +0600

    Add Exit alert Dialog

--- lib/Screens/Home/home_sceen.dart
+++ lib/Screens/Home/home_sceen.dart
@@ -19,35 +19,61 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     // Enable hybrid composition.
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
- 
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SR-Medicine'),
-      ),
-      drawer: Nav_Drawer(),
-      body: Column(
-        children: [
-          LinearProgressIndicator(
-            value: progress,
-            color: Colors.redAccent,
-            backgroundColor: Colors.black12,
-          ),
-          Expanded(
-            child: WebView(
+    return WillPopScope(
+      onWillPop: showExitPopup,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('SR-Medicine'),
+        ),
+        drawer: Nav_Drawer(),
+        body: Column(
+          children: [
+            LinearProgressIndicator(
+              value: progress,
+              color: Colors.redAccent,
+              backgroundColor: Colors.black12,
+            ),
+            Expanded(
+              child: WebView(
                 initialUrl: 'https://shahanarahmanmedicine.com/shop/',
                 javascriptMode: JavascriptMode.unrestricted,
-              
-
-                onProgress: (progress) => setState(() {this.progress = progress/100;}),
+                onProgress: (progress) => setState(() {
+                  this.progress = progress / 100;
+                }),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
+  Future<bool> showExitPopup() async {
+    return await showDialog(
+          //show confirm dialogue
+          //the return value will be from "Yes" or "No" options
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Exit App'),
+            content: Text('Do you want to exit an App?'),
+            actions: [
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                //return false when click on "NO"
+                child: Text('No'),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                //return true when click on "Yes"
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        ) ??
+        false; //if showDialouge had returned null, then return false
+  }
 }
